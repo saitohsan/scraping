900afa618876d1c2fc5f7e089705d7341e3817bd
saitohsan <saitohsan@muc.biglobe.ne.jp>
2023-09-08 11:04:13 +0900

    Create sample.py

---
+++ comdesklead/sample.py
@@ -0,0 +1,20 @@
+import csv
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome import service as fs
+
+# chromeドライバのパス指定
+driver_path = "C:\Program Files\chromedriver"
+chrome_service = fs.Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=chrome_service)
+
+# 探索するURLを指定する
+url = "https://www.tripadvisor.jp/Restaurants-g298207-zft21269-Fukuoka_Fukuoka_Prefecture_Kyushu.html"
+driver.get(url)
+
+
+# driver quit
+driver.quit()
