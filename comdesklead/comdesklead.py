c249aae3922a75551b173a86ff1e2b161d971d23
saitohsan <saitohsan@muc.biglobe.ne.jp>
2023-09-12 17:48:20 +0900

    Update comdesklead.py

--- comdesklead/comdesklead.py
+++ comdesklead/comdesklead.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome import service as fs
+from selenium.webdriver.chrome.service import Service
 
 # chrome起動設定
 chrome_options = webdriver.chrome.options.Options()
@@ -13,7 +13,8 @@ chrome_options = webdriver.chrome.options.Options()
 chrome_options.binary_location = r'C:\Program Files\chromebinary\chrome.exe'
 driver_path = r'C:\Program Files\chromedriver\chromedriver.exe'
 
-driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+service = Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # 探索するURLを指定する
 url = "https://usengroup.comdesk.com/auth"
