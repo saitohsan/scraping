21e61ce274ef83c0f29eba74d86d6644de5935e8
saitohsan <saitohsan@muc.biglobe.ne.jp>
2023-09-27 17:55:47 +0900

    Update comdesklead.py

--- comdesklead/comdesklead.py
+++ comdesklead/comdesklead.py
@@ -116,9 +116,138 @@ path2 = 'C:/Users/tak-saito/Downloads/indeed_calllog.csv'
 os.rename(path1,path2)
 
 # 処理2周目
+# Nurturing
 driver.get(history_url)
 
 sleep(10)
+
+# 今日の日付を入力
+# 入力欄に直突っ込みすると、書式が崩れる。年月日で分けて入力
+#input_date = datetime.datetime.now().strftime('%Y/%m/%d')
+
+date_from = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[4]/div[1]/div/div[1]/div[4]/label/div/div/div/input")
+date_from.send_keys(datetime.datetime.now().strftime('%Y'))
+date_from.send_keys(Keys.TAB)
+date_from.send_keys(datetime.datetime.now().strftime('%m'))
+#date_from.send_keys(Keys.TAB) - 月を入れると自動でTAB移動するので省略
+date_from.send_keys(datetime.datetime.now().strftime('%d'))
+
+sleep(3)
+
+date_to = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/div[1]/div/div[1]/div[5]/label/div/div/div/input')
+date_to.send_keys(datetime.datetime.now().strftime('%Y'))
+date_to.send_keys(Keys.TAB)
+date_to.send_keys(datetime.datetime.now().strftime('%m'))
+#date_to.send_keys(Keys.TAB) - 月を入れると自動でTAB移動するので省略
+date_to.send_keys(datetime.datetime.now().strftime('%d'))
+
+sleep(10)
+
+# 指定ワークグループと、DL、ローカルファイルリネームの繰り返し処理
+# Nurturing
+
+# ワークグループを1回クリック
+workgrp_btn = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/div[1]/div/div[1]/div[3]/button[1]/span[2]/p')
+workgrp_btn.click()
+
+# Nurturingをクリック
+indeed_box = driver.find_element(by=By.XPATH, value='/html/body/div[7]/div/div[1]/div/div/div[13]/div[2]')
+indeed_box.click()
+
+# 適用押下
+apply_btn = driver.find_element(by=By.XPATH, value='/html/body/div[7]/div/div[2]/button[2]/span[2]/span')
+apply_btn.click()
+
+# 描画待機 - nurturing絞り込みは時間がかかる
+sleep(20)
+
+# ワークグループを1回クリック（戻す）
+workgrp_btn = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/div[1]/div/div[1]/div[3]/button[1]/span[2]/p')
+
+# 待機
+sleep(3)
+
+# CSV DLボタンを押下
+
+csv_dl_button = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/button/span[2]')
+csv_dl_button.click()
+
+# DL待機は10秒では不足
+sleep(20)
+
+# ローカルリネーム nurturing
+# 変更前ファイル
+path1 = 'C:/Users/tak-saito/Downloads/calllog.csv'
+path2 = 'C:/Users/tak-saito/Downloads/nurturing_calllog.csv'
+os.rename(path1,path2)
+
+
+# 処理3周目
+# 日本の山水
+driver.get(history_url)
+
+sleep(10)
+
+# 今日の日付を入力
+# 入力欄に直突っ込みすると、書式が崩れる。年月日で分けて入力
+#input_date = datetime.datetime.now().strftime('%Y/%m/%d')
+
+date_from = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[4]/div[1]/div/div[1]/div[4]/label/div/div/div/input")
+date_from.send_keys(datetime.datetime.now().strftime('%Y'))
+date_from.send_keys(Keys.TAB)
+date_from.send_keys(datetime.datetime.now().strftime('%m'))
+#date_from.send_keys(Keys.TAB) - 月を入れると自動でTAB移動するので省略
+date_from.send_keys(datetime.datetime.now().strftime('%d'))
+
+sleep(3)
+
+date_to = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/div[1]/div/div[1]/div[5]/label/div/div/div/input')
+date_to.send_keys(datetime.datetime.now().strftime('%Y'))
+date_to.send_keys(Keys.TAB)
+date_to.send_keys(datetime.datetime.now().strftime('%m'))
+#date_to.send_keys(Keys.TAB) - 月を入れると自動でTAB移動するので省略
+date_to.send_keys(datetime.datetime.now().strftime('%d'))
+
+sleep(10)
+
+# 指定ワークグループと、DL、ローカルファイルリネームの繰り返し処理
+# 日本の山水
+
+# ワークグループを1回クリック
+workgrp_btn = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/div[1]/div/div[1]/div[3]/button[1]/span[2]/p')
+workgrp_btn.click()
+
+# 日本の山水をクリック
+indeed_box = driver.find_element(by=By.XPATH, value='/html/body/div[7]/div/div[1]/div/div/div[17]/div[2]')
+indeed_box.click()
+
+# 適用押下
+apply_btn = driver.find_element(by=By.XPATH, value='/html/body/div[7]/div/div[2]/button[2]/span[2]/span')
+apply_btn.click()
+
+# 描画待機
+sleep(5)
+
+# ワークグループを1回クリック（戻す）
+workgrp_btn = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/div[1]/div/div[1]/div[3]/button[1]/span[2]/p')
+
+# 待機
+sleep(3)
+
+# CSV DLボタンを押下
+
+csv_dl_button = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[4]/button/span[2]')
+csv_dl_button.click()
+
+# DL待機は10秒では不足
+sleep(20)
+
+# ローカルリネーム nurturing
+# 変更前ファイル
+path1 = 'C:/Users/tak-saito/Downloads/calllog.csv'
+path2 = 'C:/Users/tak-saito/Downloads/日本の山水_calllog.csv'
+os.rename(path1,path2)
+
 
 
 # driver quit
