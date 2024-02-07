89fe385027ad9ff4d8c54de2e9a832cfd37440df
saitohsan <saitohsan@muc.biglobe.ne.jp>
2024-02-07 16:20:42 +0900

    Update yelp.py

---
+++ yelp/yelp.py
@@ -0,0 +1,20 @@
+import csv
+import time
+import os
+import datetime
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+
+# chrome起動設定
+chrome_options = webdriver.chrome.options.Options()
+
+chrome_options.binary_location = r'C:\Program Files\chromebinary\chrome.exe'
+driver_path = r'C:\Program Files\chromedriver\chromedriver.exe'
+
+service = Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
